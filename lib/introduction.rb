d3dfaffd202e6979246c11cafc27858fbe46cb5c
4anth0ny1 <acatullo4@gmail.com>
2020-12-08 20:09:00 -0600

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
+introduction("Anthony")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language("Anthony", "Ruby")
