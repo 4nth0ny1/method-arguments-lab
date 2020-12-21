74ed4dc6425c9adde4070b7ba99ebae5afa0b419
4anth0ny1 <acatullo4@gmail.com>
2020-12-20 18:33:27 -0600

    commit tree 2b904d6762b82d23cefa4a5d03503905b920161e

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,7 @@
-require 'spec_helper'
-
 def introduction(name)
     puts "Hi, my name is #{name}."
 end
-introduction("Anthony")
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language("Anthony", "Ruby")
